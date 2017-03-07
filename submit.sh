05cc25f67d42d50972902692d45dc3a8f04ce577
Sabber Ahamed <msahamed@memphis.edu>
2017-03-07 15:55:33 -0600

    modifies submit.sh

--- submit.sh
+++ submit.sh
@@ -7,8 +7,14 @@
 #export OMP_NUM_THREADS=1
 
 # source the module command
+
 source /etc/profile.d/modules.sh
 
 cd /home/msahamed/nmdExperiemnt_combined/diffusion_DES/result
 
- ./../dynearthsol2d ./core-complex.cfg
+declare -a arr=("core_complex_deviatoric_variable_dilation" "core_complex_energy_fixed_dilation" "core_complex_energy_variable_dilation" "core_complex_energy")
+for i in "${arr[@]}"
+do
+    file_name="$i.sh"
+    qsub file_name
+done
